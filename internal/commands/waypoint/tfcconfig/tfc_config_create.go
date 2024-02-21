153feb7807fbd49acd56c71aaf0530c6e6ff816d
Teresa Chu <teresa.chu@hashicorp.com>
2024-02-21 14:34:54 -0500

    Preamble needs to end with a colon.

--- internal/commands/waypoint/tfcconfig/tfc_config_create.go
+++ internal/commands/waypoint/tfcconfig/tfc_config_create.go
@@ -1,53 +1,53 @@
 package tfcconfig
 
 import (
-	"context"
-	"fmt"
+    "context"
+    "fmt"
 
-	"github.com/hashicorp/hcp-sdk-go/clients/cloud-waypoint-service/preview/2023-08-18/client/waypoint_service"
-	"github.com/hashicorp/hcp-sdk-go/clients/cloud-waypoint-service/preview/2023-08-18/models"
-	"github.com/hashicorp/hcp/internal/pkg/cmd"
-	"github.com/hashicorp/hcp/internal/pkg/format"
-	"github.com/hashicorp/hcp/internal/pkg/heredoc"
-	"github.com/hashicorp/hcp/internal/pkg/iostreams"
-	"github.com/hashicorp/hcp/internal/pkg/profile"
+    "github.com/hashicorp/hcp-sdk-go/clients/cloud-waypoint-service/preview/2023-08-18/client/waypoint_service"
+    "github.com/hashicorp/hcp-sdk-go/clients/cloud-waypoint-service/preview/2023-08-18/models"
+    "github.com/hashicorp/hcp/internal/pkg/cmd"
+    "github.com/hashicorp/hcp/internal/pkg/format"
+    "github.com/hashicorp/hcp/internal/pkg/heredoc"
+    "github.com/hashicorp/hcp/internal/pkg/iostreams"
+    "github.com/hashicorp/hcp/internal/pkg/profile"
 )
 
 func NewCmdSet(ctx *cmd.Context, runF func(opts *TFCConfigOpts) error) *cmd.Command {
-	opts := &TFCConfigOpts{
-		Ctx:            ctx.ShutdownCtx,
-		Output:         ctx.Output,
-		Profile:        ctx.Profile,
-		IO:             ctx.IO,
-		WaypointClient: waypoint_service.New(ctx.HCP, nil),
-	}
+    opts := &TFCConfigOpts{
+        Ctx:            ctx.ShutdownCtx,
+        Output:         ctx.Output,
+        Profile:        ctx.Profile,
+        IO:             ctx.IO,
+        WaypointClient: waypoint_service.New(ctx.HCP, nil),
+    }
 
-	cmd := &cmd.Command{
-		Name:      "create",
-		ShortHelp: "Set TFC Configuration.",
-		LongHelp: heredoc.New(ctx.IO).Mustf(`{{ PreserveNewLines }}The {{Bold "hcp waypoint tfc-config create"}} command 
+    cmd := &cmd.Command{
+        Name:      "create",
+        ShortHelp: "Set TFC Configuration.",
+        LongHelp: heredoc.New(ctx.IO).Mustf(`{{ PreserveNewLines }}The {{Bold "hcp waypoint tfc-config create"}} command 
 sets the TFC Organization Name and TFC Team token that will be used in Waypoint.
 
 There can only be one TFC Config set for each HCP Project. 
 
 TFC Configs can be reviewed using the {{Bold "hcp waypoint tfc-config read" }} command
 and removed with the {{Bold "hcp waypoint tfc-config delete"}} command.{{ PreserveNewLines }}`),
-		Examples: []cmd.Example{
-			{
-				Preamble: `Create a new TFC Config in HCP Waypoint.`,
-				Command: heredoc.New(ctx.IO, heredoc.WithPreserveNewlines()).Must(`
+        Examples: []cmd.Example{
+            {
+                Preamble: `Create a new TFC Config in HCP Waypoint:`,
+                Command: heredoc.New(ctx.IO, heredoc.WithPreserveNewlines()).Must(`
 				$ hcp waypoint tfc-config create example-org <token>`),
-			},
-		},
-		Args: cmd.PositionalArguments{
-			Args: []cmd.PositionalArgument{
-				{
-					Name:          "TFC_ORG",
-					Documentation: heredoc.New(ctx.IO).Must(`Name of the Terraform Cloud Organization`),
-				},
-				{
-					Name: "TOKEN",
-					Documentation: heredoc.New(ctx.IO).Must(`{{ PreserveNewLines }}
+            },
+        },
+        Args: cmd.PositionalArguments{
+            Args: []cmd.PositionalArgument{
+                {
+                    Name:          "TFC_ORG",
+                    Documentation: heredoc.New(ctx.IO).Must(`Name of the Terraform Cloud Organization`),
+                },
+                {
+                    Name: "TOKEN",
+                    Documentation: heredoc.New(ctx.IO).Must(`{{ PreserveNewLines }}
 Terraform Cloud Team token for the TFC organization. 
 
 Team token must be set in order to perform HCP Waypoint commands. 	
@@ -59,63 +59,63 @@ HCP Waypoint requires Team level access tokens in order to run correctly.
 Please ensure that your TFCConfig token has the correct permissions.
 					{{ PreserveNewLines }}
 					`),
-				},
-			},
-		},
-		RunF: func(c *cmd.Command, args []string) error {
-			opts.TfcOrg = args[0]
-			opts.Token = args[1]
-			if runF != nil {
-				return runF(opts)
-			}
-			return createRun(opts)
-		},
-		PersistentPreRun: func(c *cmd.Command, args []string) error {
-			return cmd.RequireOrgAndProject(ctx)
-		},
-	}
-	return cmd
+                },
+            },
+        },
+        RunF: func(c *cmd.Command, args []string) error {
+            opts.TfcOrg = args[0]
+            opts.Token = args[1]
+            if runF != nil {
+                return runF(opts)
+            }
+            return createRun(opts)
+        },
+        PersistentPreRun: func(c *cmd.Command, args []string) error {
+            return cmd.RequireOrgAndProject(ctx)
+        },
+    }
+    return cmd
 }
 
 type TFCConfigOpts struct {
-	Ctx     context.Context
-	Profile *profile.Profile
-	Output  *format.Outputter
-	IO      iostreams.IOStreams
+    Ctx     context.Context
+    Profile *profile.Profile
+    Output  *format.Outputter
+    IO      iostreams.IOStreams
 
-	TfcOrg         string
-	Token          string
-	WaypointClient waypoint_service.ClientService
+    TfcOrg         string
+    Token          string
+    WaypointClient waypoint_service.ClientService
 }
 
 func createRun(opts *TFCConfigOpts) error {
-	nsID, err := GetNamespace(opts.Ctx, opts.WaypointClient, opts.Profile.OrganizationID, opts.Profile.ProjectID)
-	if err != nil {
-		return fmt.Errorf("error getting namespace: %w", err)
-	}
+    nsID, err := GetNamespace(opts.Ctx, opts.WaypointClient, opts.Profile.OrganizationID, opts.Profile.ProjectID)
+    if err != nil {
+        return fmt.Errorf("error getting namespace: %w", err)
+    }
 
-	ns := &models.HashicorpCloudWaypointRefNamespace{ID: nsID}
-	request := &models.HashicorpCloudWaypointCreateTFCConfigRequest{
-		Namespace: ns,
-		TfcConfig: &models.HashicorpCloudWaypointTFCConfig{
-			OrganizationName: opts.TfcOrg,
-			Token:            opts.Token,
-		},
-	}
-	resp, err := opts.WaypointClient.WaypointServiceCreateTFCConfig(
-		&waypoint_service.WaypointServiceCreateTFCConfigParams{
-			Body:        request,
-			NamespaceID: nsID,
-			Context:     opts.Ctx,
-		}, nil,
-	)
-	if err != nil {
-		return fmt.Errorf("error setting TFC config: %w", err)
+    ns := &models.HashicorpCloudWaypointRefNamespace{ID: nsID}
+    request := &models.HashicorpCloudWaypointCreateTFCConfigRequest{
+        Namespace: ns,
+        TfcConfig: &models.HashicorpCloudWaypointTFCConfig{
+            OrganizationName: opts.TfcOrg,
+            Token:            opts.Token,
+        },
+    }
+    resp, err := opts.WaypointClient.WaypointServiceCreateTFCConfig(
+        &waypoint_service.WaypointServiceCreateTFCConfigParams{
+            Body:        request,
+            NamespaceID: nsID,
+            Context:     opts.Ctx,
+        }, nil,
+    )
+    if err != nil {
+        return fmt.Errorf("error setting TFC config: %w", err)
 
-	}
+    }
 
-	fmt.Fprintf(opts.IO.Err(), "%s TFC Config  %q updated\n", opts.IO.ColorScheme().SuccessIcon(), resp.Payload.TfcConfig.OrganizationName)
+    fmt.Fprintf(opts.IO.Err(), "%s TFC Config  %q updated\n", opts.IO.ColorScheme().SuccessIcon(), resp.Payload.TfcConfig.OrganizationName)
 
-	return nil
+    return nil
 
 }
