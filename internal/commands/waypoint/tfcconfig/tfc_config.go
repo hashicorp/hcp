3b442e4d7b4d8688bfed1513481a9c7f33370c6c
Teresa Chu <1695478+teresamychu@users.noreply.github.com>
2024-02-21 12:13:13 -0500

    Update internal/commands/waypoint/tfcconfig/tfc_config.go
    Co-authored-by: Alex Dadgar <alex@hashicorp.com>

--- internal/commands/waypoint/tfcconfig/tfc_config.go
+++ internal/commands/waypoint/tfcconfig/tfc_config.go
@@ -11,7 +11,7 @@ import (
 func NewCmdTFCConfig(ctx *cmd.Context) *cmd.Command {
 	cmd := &cmd.Command{
 		Name:      "tfc-config",
-		ShortHelp: "Manage Terraform Cloud Configurations",
+		ShortHelp: "Manage Terraform Cloud Configurations.",
 		LongHelp: heredoc.New(ctx.IO).Must(`
 		Manage the set of TFC Configs. New TFC Configs can be created using {{ Bold "hcp waypoint tfc-config set" }}
 		and existing profiles can be viewed using {{ Bold "hcp waypoint tfc-config get" }}.
