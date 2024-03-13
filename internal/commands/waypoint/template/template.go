88b6cded8ed95ca2d7cd86e1cc0082a11807b761
paladin-devops <83741749+paladin-devops@users.noreply.github.com>
2024-03-13 10:56:27 -0400

    [WAYP-2332] waypoint: Add template create command.

---
+++ internal/commands/waypoint/template/template.go
@@ -0,0 +1,46 @@
+package template
+
+import (
+	"github.com/hashicorp/hcp/internal/commands/waypoint/opts"
+	"github.com/hashicorp/hcp/internal/pkg/cmd"
+	"github.com/hashicorp/hcp/internal/pkg/heredoc"
+)
+
+type TemplateOpts struct {
+	opts.WaypointOpts
+
+	Name                       string
+	Summary                    string
+	Description                string
+	ReadmeMarkdownTemplateFile string
+	Labels                     []string
+	Tags                       map[string]string
+
+	TerraformNoCodeModuleSource  string
+	TerraformNoCodeModuleVersion string
+	TerraformCloudProjectName    string
+	TerraformCloudProjectID      string
+
+	// testFunc is used for testing, so that the command can be tested without
+	// using the real API.
+	testFunc func(c *cmd.Command, args []string) error
+}
+
+func NewCmdTemplate(ctx *cmd.Context) *cmd.Command {
+	opts := &TemplateOpts{
+		WaypointOpts: opts.New(ctx),
+	}
+
+	cmd := &cmd.Command{
+		Name:      "template",
+		ShortHelp: "Manage HCP Waypoint templates.",
+		LongHelp: heredoc.New(ctx.IO).Must(`
+		The {{ Bold "hcp waypoint template" }} commands manage templates. A
+		template is a reusable configuration for creating applications.
+		`),
+	}
+
+	cmd.AddChild(NewCmdCreate(ctx, opts))
+
+	return cmd
+}
---
+++ internal/commands/waypoint/template/template_create_test.go
@@ -0,0 +1,105 @@
+package template
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-openapi/runtime/client"
+	"github.com/hashicorp/hcp/internal/pkg/cmd"
+	"github.com/hashicorp/hcp/internal/pkg/format"
+	"github.com/hashicorp/hcp/internal/pkg/iostreams"
+	"github.com/hashicorp/hcp/internal/pkg/profile"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCmdTemplateCreate(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		Name    string
+		Args    []string
+		Profile func(t *testing.T) *profile.Profile
+		Error   string
+		Expect  *TemplateOpts
+	}{
+		{
+			Name:    "No Org",
+			Profile: profile.TestProfile,
+			Args:    []string{},
+			Error:   "Organization ID must be configured",
+		},
+		{
+			Name: "no args",
+			Profile: func(t *testing.T) *profile.Profile {
+				return profile.TestProfile(t).SetOrgID("123")
+			},
+			Args:  []string{},
+			Error: "accepts 1 arg(s), received 0",
+		},
+		{
+			Name: "happy",
+			Profile: func(t *testing.T) *profile.Profile {
+				return profile.TestProfile(t).SetOrgID("123")
+			},
+			Args: []string{
+				"--name=cli-test",
+				"-s", "A template created using the CLI.",
+				"--terraform-cloud-project-id", "prj-abcdefghij",
+				"--terraform-cloud-project-name", "test",
+				"--terraform-no-code-module-source", "private/waypoint/waypoint-nocode-module/null",
+				"--terraform-no-code-module-version", "0.0.1",
+				"-l", "cli",
+				"-d", "A template created with the CLI.",
+				"-t", "cli=true",
+				"--readme-markdown-template-file", "readme_test.txt",
+			},
+			Expect: &TemplateOpts{
+				Name:                         "cli-test",
+				Summary:                      "A template created using the CLI.",
+				Description:                  "A template created with the CLI.",
+				TerraformCloudProjectID:      "prj-abcdefghij",
+				TerraformCloudProjectName:    "test",
+				TerraformNoCodeModuleSource:  "private/waypoint/waypoint-nocode-module/null",
+				TerraformNoCodeModuleVersion: "0.0.1",
+				ReadmeMarkdownTemplateFile:   "readme_test.txt",
+				Labels:                       []string{"cli"},
+				Tags:                         map[string]string{"cli": "true"},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.Name, func(t *testing.T) {
+			t.Parallel()
+
+			r := require.New(t)
+
+			// Create a context.
+			io := iostreams.Test()
+			ctx := &cmd.Context{
+				IO:          io,
+				Profile:     c.Profile(t),
+				Output:      format.New(io),
+				HCP:         &client.Runtime{},
+				ShutdownCtx: context.Background(),
+			}
+
+			var tplOpts TemplateOpts
+			tplOpts.testFunc = func(c *cmd.Command, args []string) error {
+				return nil
+			}
+			cmd := NewCmdCreate(ctx, &tplOpts)
+			cmd.SetIO(io)
+
+			cmd.Run(c.Args)
+
+			if c.Expect != nil {
+				r.NotNil(c.Expect)
+
+				r.Equal(c.Expect.Name, tplOpts.Name)
+				r.Equal(c.Expect.Description, tplOpts.Description)
+			}
+		})
+	}
+}
