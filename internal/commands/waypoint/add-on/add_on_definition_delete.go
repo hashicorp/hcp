eb837fe3ca77d9600620f8517a33671771b120e3
paladin-devops <83741749+paladin-devops@users.noreply.github.com>
2024-03-21 15:36:41 -0400

    [WAYP-2346] waypoint: Add add-on definition delete command.

---
+++ internal/commands/waypoint/add-on/add_on_definition_delete.go
@@ -0,0 +1,70 @@
+package addon
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/hcp-sdk-go/clients/cloud-waypoint-service/preview/2023-08-18/client/waypoint_service"
+	"github.com/hashicorp/hcp/internal/pkg/cmd"
+	"github.com/hashicorp/hcp/internal/pkg/flagvalue"
+	"github.com/pkg/errors"
+)
+
+func NewCmdAddOnDefinitionDelete(ctx *cmd.Context, opts *AddOnDefinitionOpts) *cmd.Command {
+	cmd := &cmd.Command{
+		Name:      "delete",
+		ShortHelp: "Delete an HCP Waypoint add-on definition.",
+		LongHelp: "Delete an HCP Waypoint add-on definition. This will delete the add-on definition" +
+			" with the provided name.",
+		Examples: []cmd.Example{
+			{
+				Preamble: "Delete an HCP Waypoint add-on definition:",
+				Command:  "hcp waypoint add-ons definitions delete -n my-addon-definition",
+			},
+		},
+		RunF: func(c *cmd.Command, args []string) error {
+			if opts.testFunc != nil {
+				return opts.testFunc(c, args)
+			}
+			return addOnDefinitionDelete(opts)
+		},
+		PersistentPreRun: func(c *cmd.Command, args []string) error {
+			return cmd.RequireOrgAndProject(ctx)
+		},
+		Flags: cmd.Flags{
+			Local: []*cmd.Flag{
+				{
+					Name:         "name",
+					Shorthand:    "n",
+					DisplayValue: "NAME",
+					Description:  "The name of the add-on definition to be deleted.",
+					Value:        flagvalue.Simple("", &opts.Name),
+					Required:     true,
+				},
+			},
+		},
+	}
+
+	return cmd
+}
+
+func addOnDefinitionDelete(opts *AddOnDefinitionOpts) error {
+	ns, err := opts.Namespace()
+	if err != nil {
+		return err
+	}
+
+	_, err = opts.WS.WaypointServiceDeleteAddOnDefinition2(
+		&waypoint_service.WaypointServiceDeleteAddOnDefinition2Params{
+			NamespaceID:         ns.ID,
+			Context:             opts.Ctx,
+			AddOnDefinitionName: opts.Name,
+		}, nil,
+	)
+	if err != nil {
+		return errors.Wrapf(err, "failed to delete add-on definition %q", opts.Name)
+	}
+
+	fmt.Fprintf(opts.IO.Err(), "Add-on definition %q deleted.", opts.Name)
+
+	return nil
+}
---
+++ internal/commands/waypoint/add-on/add_on_definition_list.go
@@ -0,0 +1,76 @@
+package addon
+
+import (
+	"github.com/hashicorp/hcp-sdk-go/clients/cloud-waypoint-service/preview/2023-08-18/client/waypoint_service"
+	"github.com/hashicorp/hcp-sdk-go/clients/cloud-waypoint-service/preview/2023-08-18/models"
+	"github.com/hashicorp/hcp/internal/pkg/cmd"
+	"github.com/hashicorp/hcp/internal/pkg/format"
+	"github.com/pkg/errors"
+)
+
+func NewCmdAddOnDefinitionList(ctx *cmd.Context, opts *AddOnDefinitionOpts) *cmd.Command {
+	cmd := &cmd.Command{
+		Name:      "list",
+		ShortHelp: "List all known HCP Waypoint add-on definitions.",
+		LongHelp:  "List all known add-on definitions for HCP Waypoint.",
+		Examples: []cmd.Example{
+			{
+				Preamble: "List all known HCP Waypoint add-on definitions:",
+				Command:  "hcp waypoint add-ons definitions list",
+			},
+		},
+		RunF: func(c *cmd.Command, args []string) error {
+			if opts.testFunc != nil {
+				return opts.testFunc(c, args)
+			}
+			return addOnDefinitionsList(opts)
+		},
+		PersistentPreRun: func(c *cmd.Command, args []string) error {
+			return cmd.RequireOrgAndProject(ctx)
+		},
+	}
+
+	return cmd
+}
+
+func addOnDefinitionsList(opts *AddOnDefinitionOpts) error {
+	ns, err := opts.Namespace()
+	if err != nil {
+		return err
+	}
+
+	var addOnDefinitions []*models.HashicorpCloudWaypointAddOnDefinition
+
+	listResp, err := opts.WS.WaypointServiceListAddOnDefinitions(
+		&waypoint_service.WaypointServiceListAddOnDefinitionsParams{
+			NamespaceID: ns.ID,
+			Context:     opts.Ctx,
+		}, nil,
+	)
+	if err != nil {
+		return errors.Wrap(err, "failed to list add-on definitions")
+	}
+
+	addOnDefinitions = append(addOnDefinitions, listResp.GetPayload().AddOnDefinitions...)
+
+	for listResp.GetPayload().Pagination.NextPageToken != "" {
+		listResp, err = opts.WS.WaypointServiceListAddOnDefinitions(
+			&waypoint_service.WaypointServiceListAddOnDefinitionsParams{
+				NamespaceID:             ns.ID,
+				Context:                 opts.Ctx,
+				PaginationNextPageToken: &listResp.GetPayload().Pagination.NextPageToken,
+			}, nil)
+		if err != nil {
+			return errors.Wrapf(err, "failed to list paginated add-on definitions")
+		}
+
+		addOnDefinitions = append(addOnDefinitions, listResp.GetPayload().AddOnDefinitions...)
+	}
+
+	// This is done so that we don't panic on Output.Show if there are no add-on definitions
+	if len(addOnDefinitions) == 0 {
+		return nil
+	}
+
+	return opts.Output.Show(addOnDefinitions, format.Pretty)
+}
