892d014957907d5db6f5caf9e0b4b92c972ff275
paladin-devops <83741749+paladin-devops@users.noreply.github.com>
2024-04-02 15:38:12 -0400

    [WAYP-2340] waypoint: Add HCP Waypoint command to create add-ons.

--- internal/commands/waypoint/add-ons/add_on.go
+++ internal/commands/waypoint/add-ons/add_on.go
@@ -2,9 +2,20 @@ package addons
 
 import (
 	"github.com/hashicorp/hcp/internal/commands/waypoint/add-ons/definitions"
+	"github.com/hashicorp/hcp/internal/commands/waypoint/opts"
 	"github.com/hashicorp/hcp/internal/pkg/cmd"
 	"github.com/hashicorp/hcp/internal/pkg/heredoc"
 )
+
+type AddOnOpts struct {
+	opts.WaypointOpts
+
+	Name                string
+	AddOnDefinitionName string
+	ApplicationName     string
+
+	testFunc func(c *cmd.Command, args []string) error
+}
 
 func NewCmdAddOn(ctx *cmd.Context) *cmd.Command {
 	cmd := &cmd.Command{
@@ -18,5 +29,10 @@ manage HCP Waypoint add-ons and add-on definitions.
 
 	cmd.AddChild(definitions.NewCmdAddOnDefinition(ctx))
 
+	opts := &AddOnOpts{
+		WaypointOpts: opts.New(ctx),
+	}
+
+	cmd.AddChild(NewCmdCreate(ctx, opts))
 	return cmd
 }
---
+++ internal/commands/waypoint/add-ons/create_test.go
@@ -0,0 +1,89 @@
+package addons
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-openapi/runtime/client"
+	"github.com/hashicorp/hcp/internal/pkg/cmd"
+	"github.com/hashicorp/hcp/internal/pkg/format"
+	"github.com/hashicorp/hcp/internal/pkg/iostreams"
+	"github.com/hashicorp/hcp/internal/pkg/profile"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewCmdCreate(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		Name    string
+		Args    []string
+		Profile func(t *testing.T) *profile.Profile
+		Error   string
+		Expect  *AddOnOpts
+	}{
+		{
+			Name:    "No Org",
+			Profile: profile.TestProfile,
+			Args:    []string{},
+			Error:   "Organization ID must be configured",
+		},
+		{
+			Name: "no args",
+			Profile: func(t *testing.T) *profile.Profile {
+				return profile.TestProfile(t).SetOrgID("123")
+			},
+			Args:  []string{},
+			Error: "accepts 1 arg(s), received 0",
+		},
+		{
+			Name: "happy",
+			Profile: func(t *testing.T) *profile.Profile {
+				return profile.TestProfile(t).SetOrgID("123")
+			},
+			Args: []string{
+				"-n=cli-test",
+				"--application-name=testApp",
+				"--add-on-definition-name=testAddOnDefinition",
+			},
+			Expect: &AddOnOpts{
+				Name:                "cli-test",
+				ApplicationName:     "testApp",
+				AddOnDefinitionName: "testAddOnDefinition",
+			},
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.Name, func(t *testing.T) {
+			t.Parallel()
+
+			r := require.New(t)
+
+			io := iostreams.Test()
+			ctx := &cmd.Context{
+				IO:          io,
+				Profile:     c.Profile(t),
+				ShutdownCtx: context.Background(),
+				HCP:         &client.Runtime{},
+				Output:      format.New(io),
+			}
+
+			var addOnOpts AddOnOpts
+			addOnOpts.testFunc = func(c *cmd.Command, args []string) error {
+				return nil
+			}
+			cmd := NewCmdCreate(ctx, &addOnOpts)
+			cmd.SetIO(io)
+
+			cmd.Run(c.Args)
+
+			if c.Expect != nil {
+				r.Equal(c.Expect.Name, addOnOpts.Name)
+				r.Equal(c.Expect.ApplicationName, addOnOpts.ApplicationName)
+				r.Equal(c.Expect.AddOnDefinitionName, addOnOpts.AddOnDefinitionName)
+			}
+		})
+	}
+}
