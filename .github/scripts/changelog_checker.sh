26dfb26c87390a0cbc96869e7b341db872df6a48
Alex Dadgar <alex.dadgar@gmail.com>
2024-04-04 09:56:32 -0700

    Add copywrite headers

--- .github/scripts/changelog_checker.sh
+++ .github/scripts/changelog_checker.sh
@@ -1,3 +1,6 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
 set -euo pipefail
 
 # check if there is a diff in the .changelog directory
--- internal/pkg/ld/suggest.go
+++ internal/pkg/ld/suggest.go
@@ -1,3 +1,6 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 // package ld implements levenshtein distance in order to provide suggestions
 // based on similarity.
 package ld
