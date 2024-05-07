e02b6cb7242fb2ff753035a3658a17f8122c087a
Mercedes Hall <mercedes.hall@hashicorp.com>
2024-05-07 12:57:11 -0500

    adds RunF for now

--- internal/commands/vaultsecrets/vault_secrets.go
+++ internal/commands/vaultsecrets/vault_secrets.go
@@ -11,13 +11,18 @@ import (
 func NewCmdVaultSecrets(ctx *cmd.Context) *cmd.Command {
 	cmd := &cmd.Command{
 		Name:      "vault-secrets",
-		ShortHelp: "Manage Vault Secrets (Beta).",
+		ShortHelp: "Manage Vault Secrets in Beta.",
 		LongHelp: heredoc.New(ctx.IO).Must(`
 		The {{ template "mdCodeOrBold" "hcp vault-secrets" }} command group lets you manage HCP Vault Secrets
 		resources through the CLI (in Beta). 
 		`),
 		Aliases: []string{
 			"vault-secrets-beta",
+		},
+		// Validation rules requires either RunF or Children are set
+		// RunF can be removed when apps and secrets children are added
+		RunF: func(c *cmd.Command, args []string) error {
+			return nil
 		},
 	}
 
