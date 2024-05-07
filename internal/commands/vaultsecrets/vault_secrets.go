fd236506c7aeed6a4c77aaec42ef4fd138637970
Mercedes Hall <mercedes.hall@hashicorp.com>
2024-05-07 11:51:26 -0500

    adds top level vault secrets command

---
+++ internal/commands/vaultsecrets/vault_secrets.go
@@ -0,0 +1,25 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package vaultsecrets
+
+import (
+	"github.com/hashicorp/hcp/internal/pkg/cmd"
+	"github.com/hashicorp/hcp/internal/pkg/heredoc"
+)
+
+func NewCmdVaultSecrets(ctx *cmd.Context) *cmd.Command {
+	cmd := &cmd.Command{
+		Name:      "vault-secrets",
+		ShortHelp: "Manage Vault Secrets (Beta).",
+		LongHelp: heredoc.New(ctx.IO).Must(`
+		The {{ template "mdCodeOrBold" "hcp vault-secrets" }} command group lets you manage HCP Vault Secrets
+		resources through the CLI (in Beta). 
+		`),
+		Aliases: []string{
+			"vault-secrets-beta",
+		},
+	}
+
+	return cmd
+}
