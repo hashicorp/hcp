c1fdca29ad4302e56521994e559e1faddfe9e0a5
Mercedes Hall <mercedes.hall@hashicorp.com>
2024-05-10 15:47:46 -0500

    Adds app create command

---
+++ internal/commands/vaultsecrets/apps/create.go
@@ -0,0 +1,121 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package apps
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/go-hclog"
+
+	secretsvcpreview "github.com/hashicorp/hcp-sdk-go/clients/cloud-vault-secrets/preview/2023-11-28/client/secret_service"
+	secretsvcstable "github.com/hashicorp/hcp-sdk-go/clients/cloud-vault-secrets/stable/2023-06-13/client/secret_service"
+	"github.com/hashicorp/hcp/internal/pkg/cmd"
+	"github.com/hashicorp/hcp/internal/pkg/flagvalue"
+	"github.com/hashicorp/hcp/internal/pkg/format"
+	"github.com/hashicorp/hcp/internal/pkg/heredoc"
+	"github.com/hashicorp/hcp/internal/pkg/iostreams"
+	"github.com/hashicorp/hcp/internal/pkg/profile"
+)
+
+type CreateOpts struct {
+	Ctx     context.Context
+	Profile *profile.Profile
+	Output  *format.Outputter
+	IO      iostreams.IOStreams
+	Logger  hclog.Logger
+
+	AppName       string
+	Description   string
+	StableClient  secretsvcstable.ClientService
+	PreviewClient secretsvcpreview.ClientService
+}
+
+func NewCmdCreate(ctx *cmd.Context, runF func(*CreateOpts) error) *cmd.Command {
+	opts := &CreateOpts{
+		Ctx:           ctx.ShutdownCtx,
+		Profile:       ctx.Profile,
+		Output:        ctx.Output,
+		IO:            ctx.IO,
+		StableClient:  secretsvcstable.New(ctx.HCP, nil),
+		PreviewClient: secretsvcpreview.New(ctx.HCP, nil),
+	}
+
+	cmd := &cmd.Command{
+		Name:      "create",
+		ShortHelp: "Create a new HCP Vault Secrets application.",
+		LongHelp: heredoc.New(ctx.IO).Must(`
+		The {{ template "mdCodeOrBold" "hcp vault-secrets apps create" }} command creates a new HCP Vault Secrets application.
+
+		Once an application is created, secrets lifecycle can be managed using the
+		{{ template "mdCodeOrBold" "hcp vault-secrets secret" }} command group.
+		`),
+		Examples: []cmd.Example{
+			{
+				Preamble: `Create a new application under HCP Vault Secrets:`,
+				Command: heredoc.New(ctx.IO, heredoc.WithPreserveNewlines()).Must(`
+				$ hcp vault-secrets apps create company-card \
+				  --description "Stores corporate card info."
+				`),
+			},
+		},
+		Args: cmd.PositionalArguments{
+			Args: []cmd.PositionalArgument{
+				{
+					Name:          "APP_NAME",
+					Documentation: "The name of the app to create.",
+				},
+			},
+		},
+		Flags: cmd.Flags{
+			Local: []*cmd.Flag{
+				{
+					Name:         "description",
+					DisplayValue: "DESCRIPTION",
+					Description:  "An optional description for the app to create.",
+					Value:        flagvalue.Simple("", &opts.Description),
+					Required:     false,
+				},
+			},
+		},
+		RunF: func(c *cmd.Command, args []string) error {
+			opts.AppName = args[0]
+			opts.Logger = c.Logger()
+
+			if runF != nil {
+				return runF(opts)
+			}
+			return appCreate(opts)
+		},
+		PersistentPreRun: func(c *cmd.Command, args []string) error {
+			return cmd.RequireOrgAndProject(ctx)
+		},
+	}
+
+	return cmd
+}
+
+func appCreate(opts *CreateOpts) error {
+	resp, err := opts.StableClient.CreateApp(&secretsvcstable.CreateAppParams{
+		Context:                opts.Ctx,
+		LocationProjectID:      opts.Profile.ProjectID,
+		LocationOrganizationID: opts.Profile.OrganizationID,
+		Body: secretsvcstable.CreateAppBody{
+			Name:        opts.AppName,
+			Description: opts.Description,
+		},
+	}, nil)
+
+	if err != nil {
+		opts.Logger.Info("error creating hvs app", err)
+		return fmt.Errorf("%s failed to create application with name: %s - %s", opts.IO.ColorScheme().FailureIcon(), opts.AppName, err)
+	}
+
+	if opts.Output.GetFormat() == format.Unset {
+		fmt.Fprintf(opts.IO.Err(), "%s Created application with name: %s\n", opts.IO.ColorScheme().SuccessIcon(), opts.AppName)
+		return nil
+	}
+
+	return opts.Output.Display(newDisplayer(true, resp.Payload.App))
+}
---
+++ internal/commands/vaultsecrets/apps/create_test.go
@@ -0,0 +1,104 @@
+package apps
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-openapi/runtime/client"
+	"github.com/stretchr/testify/require"
+	
+	"github.com/hashicorp/hcp/internal/pkg/cmd"
+	"github.com/hashicorp/hcp/internal/pkg/format"
+	"github.com/hashicorp/hcp/internal/pkg/iostreams"
+	"github.com/hashicorp/hcp/internal/pkg/profile"
+)
+
+func TestNewCmdCreate(t *testing.T) {
+	cases := []struct {
+		Name    string
+		Args    []string
+		Profile func(t *testing.T) *profile.Profile
+		Error   string
+		Expect  *CreateOpts
+	}{
+		{
+			Name: "Good",
+			Profile: func(t *testing.T) *profile.Profile {
+				return profile.TestProfile(t).SetOrgID("123").SetProjectID("abc")
+			},
+			Args: []string{"company-card"},
+			Expect: &CreateOpts{
+				AppName: "company-card",
+			},
+		},
+		{
+			Name: "Good with description",
+			Profile: func(t *testing.T) *profile.Profile {
+				return profile.TestProfile(t).SetOrgID("123").SetProjectID("abc")
+			},
+			Args: []string{"company-card", "--description", "Stores corporate card info."},
+			Expect: &CreateOpts{
+				AppName:     "company-card",
+				Description: "Stores corporate card info.",
+			},
+		},
+		{
+			Name:    "No Org or Project ID",
+			Profile: profile.TestProfile,
+			Args:    []string{},
+			Error:   "Organization ID and Project ID must be configured before running the command.",
+		},
+		{
+			Name: "No args",
+			Profile: func(t *testing.T) *profile.Profile {
+				return profile.TestProfile(t).SetOrgID("123").SetProjectID("abc")
+			},
+			Args:  []string{},
+			Error: "accepts 1 arg(s), received 0",
+		},
+		{
+			Name: "Too many args",
+			Profile: func(t *testing.T) *profile.Profile {
+				return profile.TestProfile(t).SetOrgID("123").SetProjectID("abc")
+			},
+			Args:  []string{"company-card", "additional-arg"},
+			Error: "ERROR: accepts 1 arg(s), received 2",
+		},
+	}
+	for _, c := range cases {
+		c := c
+		t.Run(c.Name, func(t *testing.T) {
+			t.Parallel()
+
+			r := require.New(t)
+			io := iostreams.Test()
+
+			ctx := &cmd.Context{
+				IO:          io,
+				Profile:     c.Profile(t),
+				ShutdownCtx: context.Background(),
+				HCP:         &client.Runtime{},
+				Output:      format.New(io),
+			}
+
+			var createOpts *CreateOpts
+			createCmd := NewCmdCreate(ctx, func(o *CreateOpts) error {
+				createOpts = o
+				return nil
+			})
+			createCmd.SetIO(io)
+
+			code := createCmd.Run(c.Args)
+			if c.Error != "" {
+				r.NotZero(code)
+				r.Contains(io.Error.String(), c.Error)
+				return
+			}
+
+			r.Zero(code, io.Error.String())
+			r.NotNil(createOpts)
+			r.Equal(c.Expect.AppName, createOpts.AppName)
+			r.Equal(c.Expect.Description, createOpts.Description)
+		})
+	}
+}
