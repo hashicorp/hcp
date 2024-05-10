e771998306feebf9c7d8190b9ecbce073e1648c3
Gaurav Sharma <gaurav@hashicorp.com>
2024-05-10 18:38:42 -0400

    save work

--- internal/commands/vaultsecrets/secrets/secrets.go
+++ internal/commands/vaultsecrets/secrets/secrets.go
@@ -5,7 +5,12 @@ package secrets
 
 import (
 	"github.com/hashicorp/hcp/internal/pkg/cmd"
+	"github.com/hashicorp/hcp/internal/pkg/flagvalue"
 	"github.com/hashicorp/hcp/internal/pkg/heredoc"
+)
+
+var (
+	appName string
 )
 
 func NewCmdSecrets(ctx *cmd.Context) *cmd.Command {
@@ -16,6 +21,23 @@ func NewCmdSecrets(ctx *cmd.Context) *cmd.Command {
 		The {{ template "mdCodeOrBold" "hcp vault-secrets secrets" }} command group lets you
 		manage Vault Secrets application secrets.
 		`),
+		Flags: cmd.Flags{
+			Persistent: []*cmd.Flag{
+				{
+					Name:         "app-name",
+					DisplayValue: "NAME",
+					Description:  "The name of the Vault Secrets application. If not specified, then the value from the active profile will be used.",
+					Shorthand:    "a",
+					Value:        flagvalue.Simple("", &appName),
+				},
+			},
+		},
+		PersistentPreRun: func(c *cmd.Command, args []string) error {
+			if appName == "" && ctx.Profile.VaultSecrets != nil {
+				appName = ctx.Profile.VaultSecrets.AppName
+			}
+			return cmd.RequireVaultSecretsAppName(ctx, appName)
+		},
 	}
 
 	cmd.AddChild(NewCmdCreate(ctx, nil))
