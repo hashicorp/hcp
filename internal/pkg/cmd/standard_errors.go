88bc749ab817332a8754bdeec2fec4751343d5b4
Gaurav Sharma <gaurav@hashicorp.com>
2024-05-13 16:14:13 -0400

    add back

--- internal/pkg/cmd/standard_errors.go
+++ internal/pkg/cmd/standard_errors.go
@@ -28,3 +28,26 @@ func RequireOrganization(ctx *Context) error {
 
 	return errors.New(help)
 }
+
+// RequireOrgAndProject requires that the profile has a set project and
+// organization ID.
+func RequireOrgAndProject(ctx *Context) error {
+	if ctx.Profile.ProjectID != "" && ctx.Profile.OrganizationID != "" {
+		return nil
+	}
+
+	cs := ctx.IO.ColorScheme()
+	help := heredoc.Docf(`%v
+
+	Please run %s to interactively set the Organization and Project ID, or run:
+
+	%v
+	%v`,
+		cs.String("Organization ID and Project ID must be configured before running the command.").Color(cs.Orange()),
+		cs.String("hcp profile init").Bold(),
+		cs.String("$ hcp profile set organization_id <organization_id>").Bold(),
+		cs.String("$ hcp profile set project_id <project_id>").Bold(),
+	)
+
+	return errors.New(help)
+}
