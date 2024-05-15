ce04176f2c25c96d696ebdaa9bcab02cbaa2f484
Gaurav Sharma <gaurav@hashicorp.com>
2024-05-15 16:30:46 -0400

    update validation returned err output

--- internal/pkg/vaultsecrets/validation.go
+++ internal/pkg/vaultsecrets/validation.go
@@ -25,13 +25,22 @@ func RequireVaultSecretsAppName(ctx *cmd.Context, appName string) error {
 	cs := ctx.IO.ColorScheme()
 	help := heredoc.Docf(`%v
 
-	Please run %s to interactively set the Vault Secrets application name, or run:
+	To set the Vault Secrets application name interactively, run:
+
+	%v
+
+	Alternatively, you can set the Vault Secrets applicaton name on the active pofile using the command:
+
+	%v
+
+	If you prefer specifying the Vault Secrets application name directly via the command line, use:
 
 	%v
 	`,
 		cs.String("Vault Secrets application name must be configured before running the command.").Color(cs.Orange()),
 		cs.String("$ hcp profile set vault-secrets/app <app_name>").Bold(),
 		cs.String("$ hcp profile init --vault-secrets").Bold(),
+		cs.String("$ hcp vault-secrets secrets --app <app_name> <sub-cmd>").Bold(),
 	)
 
 	return errors.New(help)
