77f9e07a6fe3828be45b9d0a1079552827eeed8e
Gaurav Sharma <gaurav@hashicorp.com>
2024-05-15 16:20:25 -0400

    save work

--- internal/commands/vaultsecrets/secrets/displayer.go
+++ internal/commands/vaultsecrets/secrets/displayer.go
@@ -4,13 +4,15 @@
 package secrets
 
 import (
+	preview_secret_models "github.com/hashicorp/hcp-sdk-go/clients/cloud-vault-secrets/preview/2023-11-28/models"
 	models "github.com/hashicorp/hcp-sdk-go/clients/cloud-vault-secrets/stable/2023-06-13/models"
 	"github.com/hashicorp/hcp/internal/pkg/format"
 )
 
 type displayer struct {
-	secrets []*models.Secrets20230613Secret
-	single  bool
+	secrets        []*models.Secrets20230613Secret
+	previewSecrets []*preview_secret_models.Secrets20231128Secret
+	single         bool
 }
 
 func newDisplayer(single bool, secrets ...*models.Secrets20230613Secret) *displayer {
@@ -20,11 +22,30 @@ func newDisplayer(single bool, secrets ...*models.Secrets20230613Secret) *displa
 	}
 }
 
+func newDisplayerPreview(single bool, secrets ...*preview_secret_models.Secrets20231128Secret) *displayer {
+	return &displayer{
+		previewSecrets: secrets,
+		single:         single,
+	}
+}
+
 func (d *displayer) DefaultFormat() format.Format {
 	return format.Table
 }
 
 func (d *displayer) Payload() any {
+	if d.previewSecrets != nil {
+		if d.single {
+			if len(d.previewSecrets) != 1 {
+				return nil
+			}
+
+			return d.previewSecrets[0]
+		}
+
+		return d.previewSecrets
+	}
+
 	if d.single {
 		if len(d.secrets) != 1 {
 			return nil
---
+++ internal/commands/vaultsecrets/secrets/list_test.go
@@ -0,0 +1,160 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package secrets
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-openapi/runtime/client"
+
+	"github.com/hashicorp/hcp/internal/pkg/cmd"
+	"github.com/hashicorp/hcp/internal/pkg/format"
+	"github.com/hashicorp/hcp/internal/pkg/iostreams"
+	"github.com/hashicorp/hcp/internal/pkg/profile"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewCmdList(t *testing.T) {
+	t.Parallel()
+
+	testProfile := func(t *testing.T) *profile.Profile {
+		tp := profile.TestProfile(t).SetOrgID("123").SetProjectID("456")
+		tp.VaultSecrets = &profile.VaultSecretsConf{
+			AppName: "test-app-name",
+		}
+		return tp
+	}
+
+	cases := []struct {
+		Name    string
+		Args    []string
+		Profile func(t *testing.T) *profile.Profile
+		Error   string
+		Expect  *ListOpts
+	}{
+		{
+			Name:    "Failed: No secret name arg specified",
+			Profile: testProfile,
+			Args:    []string{},
+			Error:   "ERROR: accepts 1 arg(s), received 0",
+		},
+		{
+			Name:    "Good: Secret name arg specified",
+			Profile: testProfile,
+			Args:    []string{"test"},
+			Expect: &ListOpts{
+				AppName: testProfile(t).VaultSecrets.AppName,
+			},
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.Name, func(t *testing.T) {
+			t.Parallel()
+			r := require.New(t)
+
+			// Create a context.
+			io := iostreams.Test()
+			ctx := &cmd.Context{
+				IO:          io,
+				Profile:     c.Profile(t),
+				Output:      format.New(io),
+				HCP:         &client.Runtime{},
+				ShutdownCtx: context.Background(),
+			}
+
+			var gotOpts *ListOpts
+			listCmd := NewCmdList(ctx, func(o *ListOpts) error {
+				gotOpts = o
+				gotOpts.AppName = c.Profile(t).VaultSecrets.AppName
+				return nil
+			})
+			listCmd.SetIO(io)
+
+			code := listCmd.Run(c.Args)
+			if c.Error != "" {
+				r.NotZero(code)
+				r.Contains(io.Error.String(), c.Error)
+				return
+			}
+
+			r.Zero(code, io.Error.String())
+			r.NotNil(gotOpts)
+			r.Equal(c.Expect.AppName, gotOpts.AppName)
+		})
+	}
+}
+
+// func TestDeleteRun(t *testing.T) {
+// 	t.Parallel()
+
+// 	testProfile := func(t *testing.T) *profile.Profile {
+// 		tp := profile.TestProfile(t).SetOrgID("123").SetProjectID("456")
+// 		tp.VaultSecrets = &profile.VaultSecretsConf{
+// 			AppName: "test-app-name",
+// 		}
+// 		return tp
+// 	}
+
+// 	cases := []struct {
+// 		Name    string
+// 		RespErr bool
+// 		ErrMsg  string
+// 	}{
+// 		{
+// 			Name:    "Failed: Secret not found",
+// 			RespErr: true,
+// 			ErrMsg:  "[DELETE /secrets/2023-06-13/organizations/{location.organization_id}/projects/{location.project_id}/apps/{app_name}/secrets/{secret_name}][404]DeleteAppSecret default  &{Code:5 Details:[] Message:secret not found}",
+// 		},
+// 		{
+// 			Name:    "Success: Delete secret",
+// 			RespErr: false,
+// 		},
+// 	}
+
+// 	for _, c := range cases {
+// 		c := c
+// 		t.Run(c.Name, func(t *testing.T) {
+// 			t.Parallel()
+// 			r := require.New(t)
+
+// 			io := iostreams.Test()
+// 			io.ErrorTTY = true
+// 			vs := mock_secret_service.NewMockClientService(t)
+// 			opts := &DeleteOpts{
+// 				Ctx:        context.Background(),
+// 				IO:         io,
+// 				Profile:    testProfile(t),
+// 				Output:     format.New(io),
+// 				Client:     vs,
+// 				AppName:    testProfile(t).VaultSecrets.AppName,
+// 				SecretName: "test_secret",
+// 			}
+
+// 			if c.RespErr {
+// 				vs.EXPECT().DeleteAppSecret(mock.Anything, mock.Anything).Return(nil, errors.New(c.ErrMsg)).Once()
+// 			} else {
+// 				vs.EXPECT().DeleteAppSecret(&secret_service.DeleteAppSecretParams{
+// 					LocationOrganizationID: testProfile(t).OrganizationID,
+// 					LocationProjectID:      testProfile(t).ProjectID,
+// 					AppName:                testProfile(t).VaultSecrets.AppName,
+// 					SecretName:             opts.SecretName,
+// 					Context:                opts.Ctx,
+// 				}, mock.Anything).Return(&secret_service.DeleteAppSecretOK{}, nil).Once()
+// 			}
+
+// 			// Run the command
+// 			err := deleteRun(opts)
+// 			if c.ErrMsg != "" {
+// 				r.Contains(err.Error(), c.ErrMsg)
+// 				return
+// 			}
+
+// 			r.NoError(err)
+// 			r.Equal(io.Error.String(), fmt.Sprintf("✓ Successfully deleted secret with name %q\n", opts.SecretName))
+// 		})
+// 	}
+// }
