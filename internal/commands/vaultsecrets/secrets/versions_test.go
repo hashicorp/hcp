23f69db0121a64746b683a42628b8ce90fd2dc96
Gaurav Sharma <gaurav@hashicorp.com>
2024-05-17 15:21:49 -0400

    fix

--- internal/commands/vaultsecrets/secrets/versions_test.go
+++ internal/commands/vaultsecrets/secrets/versions_test.go
@@ -193,7 +193,7 @@ func TestVersionsRun(t *testing.T) {
 			}
 
 			r.NoError(err)
-			r.Contains(io.Output.String(), "Version  Created At                     Created By ")
+			r.Contains(io.Output.String(), "Version  Created At")
 		})
 	}
 }
