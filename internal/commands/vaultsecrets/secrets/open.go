f09fcb785b575611f6e3a7831c952ed9afea7fd1
Gaurav Sharma <gaurav@hashicorp.com>
2024-05-20 13:46:48 -0400

    address feedback

--- internal/commands/vaultsecrets/secrets/open.go
+++ internal/commands/vaultsecrets/secrets/open.go
@@ -10,7 +10,7 @@ import (
 
 	preview_secret_service "github.com/hashicorp/hcp-sdk-go/clients/cloud-vault-secrets/preview/2023-11-28/client/secret_service"
 	"github.com/hashicorp/hcp-sdk-go/clients/cloud-vault-secrets/stable/2023-06-13/client/secret_service"
-	appname "github.com/hashicorp/hcp/internal/commands/vaultsecrets/secrets/helper"
+	"github.com/hashicorp/hcp/internal/commands/vaultsecrets/secrets/appname"
 	"github.com/hashicorp/hcp/internal/pkg/cmd"
 	"github.com/hashicorp/hcp/internal/pkg/flagvalue"
 	"github.com/hashicorp/hcp/internal/pkg/format"
@@ -89,18 +89,17 @@ type OpenOpts struct {
 	Client         secret_service.ClientService
 }
 
-func openRun(opts *OpenOpts) error {
+func openRun(opts *OpenOpts) (err error) {
 	var fd *os.File
-	var err error
 	if opts.OutputFilePath != "" {
-		fd, err = os.OpenFile(opts.OutputFilePath, os.O_WRONLY|os.O_CREATE, 0644)
+		fd, err = os.OpenFile(opts.OutputFilePath, os.O_WRONLY|os.O_CREATE, 0600)
 		if err != nil {
 			return fmt.Errorf("failed to open the outout file %q: %w", opts.OutputFilePath, err)
 		}
 	}
 	defer func() {
 		if opts.OutputFilePath != "" {
-			fd.Close()
+			err = fd.Close()
 		}
 	}()
 
