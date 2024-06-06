6d025cd0e932e408950ddbe04860902e3d9971f7
Jasper Milan <jasper.milan@hashicorp.com>
2024-06-06 10:47:31 -0400

    add example on how to add a group manager

--- internal/commands/iam/groups/iam/iam.go
+++ internal/commands/iam/groups/iam/iam.go
@@ -12,9 +12,16 @@ func NewCmdIAM(ctx *cmd.Context) *cmd.Command {
 	cmd := &cmd.Command{
 		Name:      "iam",
 		ShortHelp: "Manage a group's IAM policy.",
-		LongHelp: heredoc.New(ctx.IO).Must(`
-		The {{ template "mdCodeOrBold" "hcp iam groups iam" }} command group lets you
-		manage a group's IAM Policy.
+		LongHelp: heredoc.New(ctx.IO, heredoc.WithPreserveNewlines()).Must(`
+		The {{ template "mdCodeOrBold" "hcp iam groups iam" }} command group lets you manage a group's IAM Policy.
+
+		To set a member as a group manager, you can use the {{ template "mdCodeOrBold" "add-binding" }} subcommand with the {{ template "mdCodeOrBold" "roles/iam.group-manager" }} role.
+
+		$ hcp iam groups iam add-binding \
+			--group=8647ae06-ca65-467a-b72d-edba1f908fc8 \
+			--member=ef938a22-09cf-4be9-b4d0-1f4587f80f53 \
+			--role=roles/iam.group-manager
+
 		`),
 	}
 
