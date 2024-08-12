2b46d2d3c8b39d4b875763696446e08653b31e1d
Mercedes Hall <mercedes.hall@hashicorp.com>
2024-08-12 14:44:52 -0500

    adds to the secrets command list

--- internal/commands/vaultsecrets/secrets/secrets.go
+++ internal/commands/vaultsecrets/secrets/secrets.go
@@ -48,6 +48,7 @@ func NewCmdSecrets(ctx *cmd.Context) *cmd.Command {
 	cmd.AddChild(NewCmdDelete(ctx, nil))
 	cmd.AddChild(NewCmdList(ctx, nil))
 	cmd.AddChild(NewCmdOpen(ctx, nil))
+	cmd.AddChild(NewCmdRotate(ctx, nil))
 
 	cmd.AddChild(versions.NewCmdVersions(ctx))
 	return cmd
