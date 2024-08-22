5b87a9faa12e1f76f1d40bf6e4c7d4380c58b9c9
Mercedes Hall <mercedes.hall@hashicorp.com>
2024-08-22 15:37:01 -0500

    create integrations

--- internal/commands/vaultsecrets/integrations/integrations.go
+++ internal/commands/vaultsecrets/integrations/integrations.go
@@ -33,5 +33,6 @@ func NewCmdIntegrations(ctx *cmd.Context) *cmd.Command {
 	cmd.AddChild(NewCmdRead(ctx, nil))
 	cmd.AddChild(NewCmdDelete(ctx, nil))
 	cmd.AddChild(NewCmdList(ctx, nil))
+	cmd.AddChild(NewCmdCreate(ctx, nil))
 	return cmd
 }
