a224b8ef29ce1edd338eafc370d62e508ed4eb9e
Jason Pilz <jasonpilz@gmail.com>
2024-08-27 07:39:59 -0700

    Remove print auth token example using curl since the docker image no longer contains the curl binary due to CVE-2024-7264

--- internal/commands/auth/print_access_token.go
+++ internal/commands/auth/print_access_token.go
@@ -17,7 +17,7 @@ func NewCmdPrintAccessToken(ctx *cmd.Context) *cmd.Command {
 		Name:      "print-access-token",
 		ShortHelp: "Print the access token for the authenticated account.",
 		LongHelp: heredoc.New(ctx.IO).Must(`
-		The {{ template "mdCodeOrBold" "hcp auth print-access-token" }} command 
+		The {{ template "mdCodeOrBold" "hcp auth print-access-token" }} command
 		prints an access token for the currently authenticated account.
 
 		The output of this command can be used to set the {{ template "mdCodeOrBold"
@@ -27,13 +27,6 @@ func NewCmdPrintAccessToken(ctx *cmd.Context) *cmd.Command {
 			{
 				Preamble: "To print the access token:",
 				Command:  "$ hcp auth print-access-token",
-			},
-			{
-				Preamble: "To use the access token when curling an API:",
-				Command: heredoc.New(ctx.IO, heredoc.WithPreserveNewlines()).Must(`
-				$ curl https://api.cloud.hashicorp.com/iam/2019-12-10/caller-identity \
-				  --header "Authorization: Bearer $(hcp auth print-access-token)"
-				  `),
 			},
 		},
 		RunF: func(c *cmd.Command, args []string) error {
