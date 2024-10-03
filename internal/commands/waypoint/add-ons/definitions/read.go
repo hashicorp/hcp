e3b0d39be8fc3bf6a69b63b41dcdd2707fc918d1
paladin-devops <83741749+paladin-devops@users.noreply.github.com>
2024-10-03 13:28:43 -0400

    fix: Field-based add-on def read output.

--- internal/commands/waypoint/add-ons/definitions/read.go
+++ internal/commands/waypoint/add-ons/definitions/read.go
@@ -74,6 +74,26 @@ func addOnDefinitionRead(opts *AddOnDefinitionOpts) error {
 	}
 
 	getRespPayload := getResp.GetPayload()
+	if getRespPayload.AddOnDefinition == nil {
+		return errors.Errorf(
+			"%s empty add-on definition returned for name %q",
+			opts.IO.ColorScheme().FailureIcon(),
+			opts.Name,
+		)
+	}
+	addOnDef := getRespPayload.AddOnDefinition
 
-	return opts.Output.Show(getRespPayload.AddOnDefinition, format.Pretty)
+	fields := []format.Field{
+		format.NewField("ID", "{{ .ID }}"),
+		format.NewField("Name", "{{ .Name }}"),
+		format.NewField("Summary", "{{ .Summary }}"),
+		format.NewField("Description", "{{ .Description }}"),
+		format.NewField("Labels", "{{ .Labels }}"),
+		format.NewField("Terraform Cloud Workspace Details", "{{ .TerraformCloudWorkspaceDetails }}"),
+		format.NewField("Module Source", "{{ .ModuleSource }}"),
+		format.NewField("Execution Mode", "{{ .TfExecutionMode }}"),
+		format.NewField("Agent Pool ID", "{{ .TfAgentPoolID }}"),
+	}
+
+	return opts.Output.Display(format.NewDisplayer(addOnDef, format.Pretty, fields))
 }
